e5c543b08e88ad63a60abc5ceb11767680cc58eb
Bruno Menezes <brunomenezesmg@gmail.com>
2018-02-25 17:24:13 -0300

    Code cleanup

--- ProlexNetSetupV2/Views/MainWindow.xaml.cs
+++ ProlexNetSetupV2/Views/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System.Net;
+﻿using System.Net;
 using System.Windows;
 
 namespace ProlexNetSetupV2.Views
@@ -7,7 +6,7 @@ namespace ProlexNetSetupV2.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
